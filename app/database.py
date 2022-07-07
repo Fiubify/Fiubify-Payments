5441fe8488716d32270870c8df1179739924fb6e
pablo.berrotaran <berrotaran.p@gmail.com>
2022-07-07 15:14:23 -0300

    Conteinerize

--- app/database.py
+++ app/database.py
@@ -1,19 +1,12 @@
 import motor.motor_asyncio
 import os
 
-'''
-mongodb_address = os.environ.get("MONGODB_URI", "mongodb://mongodb:27017")
-
 if "MONGODB_URI" in os.environ:
   mongodb_address = os.environ.get("MONGODB_URI")
   db_name = "payments-staging"
 else:
   mongodb_address = "mongodb://mongodb:27017"
   db_name = "payments-test"
-'''
-
-mongodb_address = "mongodb+srv://admin:123@fiubify-users.aybd8.mongodb.net/users-staging?retryWrites=true&w=majority"
-db_name = "payments-staging"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_address)
 db = client[db_name]
